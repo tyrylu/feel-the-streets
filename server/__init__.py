2d92e52e71f38f8dad07caeb3385b2d9ce792b96
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-02-27 00:46:37 +0100

    Actually add the server requirements. Also, add python-dotenv so we can load the .env file with some more sensitive variables.

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import dotenv
 from .config import Config
 from .amqp_huey import AMQPHuey
 
 app = Flask(__name__)
+dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
