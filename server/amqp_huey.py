781554b4c03db9abcbc94a7a3bbc3d03380b6ff9
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-03-01 21:06:03 +0100

    Make the huey consumer actually use the queue semantics. It fixes an unbounded rabbitmq memory growth.

--- server/amqp_huey.py
+++ server/amqp_huey.py
@@ -2,6 +2,7 @@ import base64
 import contextlib
 import logging
 import time
+import threading
 import pika
 from huey.api import Huey
 from huey.constants import EmptyData
@@ -13,50 +14,50 @@ logging.getLogger("pika").setLevel(logging.WARNING)
 class AMQPStorage(BaseStorage):
     def __init__(self, name='huey', broker_url='amqp://guest:guest@localhost', **storage_kwargs):
         super(AMQPStorage, self).__init__(name)
+        self._broker_url = broker_url
         self._task_queue_name = self.name
         self._schedule_queue_name = "%s.schedule"%self.name
         self._key_value_queue_name = "%s.key_value"%self.name
-        self.connection = pika.BlockingConnection(pika.URLParameters(broker_url))
+        self._queue = []
+        self._schedule = []
+        # Note that the fact that the inter thread channel sharing even works is a slight miracle.tt
+        thread = threading.Thread(target=self._amqp_init)
+        thread.daemon = True
+        thread.start()
+    
+    def _amqp_init(self):        
+        self.connection = pika.BlockingConnection(pika.URLParameters(self._broker_url))
+        self.channel = self.connection.channel()
         self.initialize_queues()
+        self.start_consuming()
 
     def initialize_queues(self):
-        chan = self.connection.channel()
+        chan = self.channel
         chan.queue_declare(queue=self._task_queue_name, durable=True)
         chan.queue_declare(queue=self._schedule_queue_name, durable=True)
         chan.queue_declare(queue=self._key_value_queue_name, durable=True)
         chan.exchange_declare(exchange=self._schedule_queue_name, exchange_type="x-delayed-message", durable=True, arguments={"x-delayed-type": "fanout"})
         chan.queue_bind(exchange=self._schedule_queue_name, queue=self._schedule_queue_name)
-        chan.close()
+
+    def start_consuming(self):
+        self.channel.basic_qos(prefetch_count=10)
+        self.channel.basic_consume(self._handle_queue, queue=self._task_queue_name)
+        self.channel.basic_consume(self._handle_schedule, queue=self._schedule_queue_name)
+        self.channel.start_consuming()
     
-    def _all_messages_in_queue_matching(self, channel, queue_name, predicate=None, limit=None):
-        matching = []
-        resp, props, body = channel.basic_get(queue=queue_name)
-        nth = 1
-        while resp:
-            if predicate and predicate(resp, props, body):
-                predicate_match = True
-            elif not predicate:
-                predicate_match = True
-            else:
-                predicate_match = False
-            if predicate_match and (limit is not None and nth <= limit):
-                matching.append((resp, props, body))
-                nth += 1
-            resp, props, body = channel.basic_get(queue=queue_name)
-        return matching
-    
-    def _queue_size(self, queue):
-        chan = self.connection.channel()
-        resp = chan.queue_declare(queue=queue, passive=True)
-        chan.close()
-        return resp.method.message_count
+    def _handle_schedule(self, channel, method, props, body):
+        self._schedule.append((method.delivery_tag, body))
+
+    def _handle_queue(self, channel, method, props, body):
+        self._queue.append((method.delivery_tag, body))
 
     @contextlib.contextmanager
     def _key_value_storage(self, republish=False):
-        chan = self.connection.channel()
+        chan = self.channel
         resp, props, _ = chan.basic_get(queue=self._key_value_queue_name)
         if not resp:
             storage = {}
+            republish = True
         else:
             storage = props.headers or {}
         yield storage
@@ -67,74 +68,71 @@ class AMQPStorage(BaseStorage):
             props.headers = storage
             props.delivery_mode = 2
             chan.basic_publish(exchange="", routing_key=self._key_value_queue_name, body="See the headers.", properties=props)
+        else:
+            if resp:
+                chan.basic_reject(resp.delivery_tag)
 
     def enqueue(self, data):
-        chan = self.connection.channel()
+        chan = self.channel
         chan.basic_publish(exchange="", routing_key=self._task_queue_name, body=data, properties=pika.BasicProperties(delivery_mode=2))
-        chan.close()
 
     def dequeue(self):
-        chan = self.connection.channel()
-        resp, props, body = chan.basic_get(queue=self._task_queue_name, no_ack=True)
-        chan.close()
+        try:
+            tag, body = self._queue.pop(0)
+        except IndexError:
+            return None
+        self.channel.basic_ack(tag)        
         return body
     
     def unqueue(self, data):
         # Somewhat expensive, because we must perform a linear search through the entire queue.
-        chan = self.connection.channel()
-        matching = self._all_messages_in_queue_matching(chan, self._task_queue_name, lambda resp, props, body: body == data)
-        for resp, _, _ in matching:
-            chan.basic_ack(resp.delivery_tag)
-        chan.close()
+        indexes = []
+        for tag, body in self._queue:
+            if body == data:
+                self.channel.basic_ack(tag)
+        for idx in indexes:
+            del self._queue[idx]
     
     def queue_size(self):
-        return self._queue_size(self._task_queue_name)
+        return len(self._queue)
     
     def enqueued_items(self, limit=None):
-        chan = self.connection.channel()
-        matching = self._all_messages_in_queue_matching(chan, self._task_queue_name, lambda resp, props, body: True, limit=limit)
-        chan.close()
-        return [item[2] for item in matching]
+        return [item[1] for item in self._queue[:limit]]
     
     def flush_queue(self):
-        chan = self.connection.channel()
-        chan.queue_purge(self._task_queue_name)
-        chan.close()
+        self.channel.queue_purge(self._task_queue_name)
+        self._queue.clear()
 
     def add_to_schedule(self, data, ts):
         delay = ts.timestamp() - time.time()
         props = pika.BasicProperties()
         props.headers["x-delay"] = str(int(delay * 1000))
         props.delivery_mode = 2
-        chan = self.connection.channel()
-        chan.basic_publish(exchange=self._schedule_queue_name, body=data, properties=props)
-        chan.close()
+        self.channel.basic_publish(exchange=self._schedule_queue_name, body=data, properties=props)
 
     def read_schedule(self, ts):
         # We currently assume that ts represents current time. No usage pattern in the huey consumer indicates it being anything else.
-        chan = self.connection.channel()
-        items = self._all_messages_in_queue_matching(chan, self._schedule_queue_name, lambda resp, props, body: True)
+        items = self._schedule
         if not items:
             return []
-        bodies = [item[2] for item in items]
-        max_delivery_tag = max(items, key=lambda item: item[0].delivery_tag)
-        chan.basic_ack(max_delivery_tag, multiple=True)
-        chan.close()
+        bodies = [item[1] for item in items]
+        max_delivery_tag = max(items, key=lambda item: item[0])
+        self.channel.basic_ack(max_delivery_tag, multiple=True)
+        self._schedule.clear()
         return bodies
     
     def schedule_size(self):
-        return self._queue_size(self._schedule_queue_name)
+        return len(self._schedule)
 
     def scheduled_items(self, limit=None):
-        chan = self.connection.channel()
+
         matching = self._all_messages_in_queue_matching(chan, self._schedule_queue_name, lambda resp, props, body: True, limit=limit)
         chan.close()
-        return [item[2] for item in matching]
+        return [item[2] for item in self._schedule[:limit]]
     
     def flush_schedule(self):
-        chan = self.connection.channel()
-        ch.queue_purge(self._schedule_queue_name)
-        chan.close()
+        self.channel.queue_purge(self._schedule_queue_name)
+        self._schedule.clear()
 
     def put_data(self, key, value):
         value = base64.b64encode(value).decode("ascii")
