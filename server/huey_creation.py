2ef40db91e36d3dc17598594a77cebe75afe80e8
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-03-04 16:26:26 +0100

    bool("False") == True... Okay, so change the consuming condition and make the env variable work for real.

--- server/huey_creation.py
+++ server/huey_creation.py
@@ -2,4 +2,4 @@ import os
 from . import app
 
 def get_huey_kwargs():
-    return dict(broker_url=app.config["AMQP_BROKER_URL"], consume=bool(os.environ.get("CONSUME", "False")))
+    return dict(broker_url=app.config["AMQP_BROKER_URL"], consume=os.environ.get("CONSUME", "False") == "True")
