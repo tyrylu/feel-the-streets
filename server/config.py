db53c46ee2f8781d0cf3e877f22a25b52fe021a3
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-03-04 16:07:12 +0100

    We must read the nevironment variables from os.environ, because the dotenv loading logic does not use setenv.

--- server/config.py
+++ server/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(os.path.join(os.path.dirname(__file__), "server.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    AMQP_BROKER_URL = os.getenv("AMQP_BROKER_URL", "amqp://app:FeelTheStreets@trycht.cz")
+    AMQP_BROKER_URL = os.environ.get("AMQP_BROKER_URL", "amqp://app:FeelTheStreets@trycht.cz")
     ENQUEUE_RETRIES = 5
