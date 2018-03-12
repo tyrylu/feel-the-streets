a0fe4139d4cd5b90ed71374d333a4f30a969cd59
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-03-12 14:01:20 +0100

    Allow translating the entity attributes (mainly because of the search dialog and changelog generator).

--- shared/humanization_utils.py
+++ shared/humanization_utils.py
@@ -2,9 +2,10 @@ import re
 import builtins
 
 def underscored_to_words(underscored):
+    _ = getattr(builtins, "_", lambda s: s)
     underscored = underscored.replace("_", " ")
     underscored = re.sub(r"^\w", lambda m: m.group(0).upper(), underscored)
-    return underscored
+    return _(underscored)
 
 def format_class_name(name):
     return re.sub(r"([a-z\d])([A-Z])([a-z\d])", lambda m: "%s %s%s"%(m.group(1), m.group(2).lower(), m.group(3)), name)
