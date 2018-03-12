5db3a93606d133aa6589fd07dac4dc0fe9b6eb90
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-03-12 13:57:54 +0100

    Allow translating the entity names. Note that the current babel behavior means that no python translatable strings will be extracted under shared/entities, but it should not be a problem.

--- babel_extractors.py
+++ babel_extractors.py
@@ -1,4 +1,5 @@
 import re
+from shared.humanization_utils import format_class_name
 
 def extract_xrc(fileobj, keywords, comment_tags, options):
     """Extract messages from xrc files.
@@ -22,3 +23,26 @@ def extract_xrc(fileobj, keywords, comment_tags, options):
         match = matching_re.search(line)
         if match:
             yield (lineno, None, match.group(2).decode("utf-8").replace("&amp;", "&"), [])
+
+
+def extract_entity_name(fileobj, keywords, comment_tags, options):
+    """Extracts the entity names for translation.
+
+    :param fileobj: the file-like object the messages should be extracted
+                    from
+    :param keywords: a list of keywords (i.e. function names) that should
+                     be recognized as translation functions, ignored
+    :param comment_tags: a list of translator tags to search for and
+                         include in the results, ignored
+    :param options: a dictionary of additional options (optional)
+    :return: an iterator over ``(lineno, funcname, message, comments)``
+             tuples
+    :rtype: ``iterator``
+    """
+    for lineno, line in enumerate(fileobj):
+        line = line.strip().decode("UTF-8")
+        match = re.match(r"class ([a-zA-Z0-9]+)\(([a-zA-Z0-9]+)\):", line)
+        if match:
+            parent = match.group(2)
+            if parent != "BaseModel" or parent != "enum.Enum":
+                yield (lineno, "", format_class_name(match.group(1)), [])
--- shared/humanization_utils.py
+++ shared/humanization_utils.py
@@ -1,9 +1,14 @@
 import re
+import builtins
 
 def underscored_to_words(underscored):
     underscored = underscored.replace("_", " ")
     underscored = re.sub(r"^\w", lambda m: m.group(0).upper(), underscored)
     return underscored
 
+def format_class_name(name):
+    return re.sub(r"([a-z\d])([A-Z])([a-z\d])", lambda m: "%s %s%s"%(m.group(1), m.group(2).lower(), m.group(3)), name)
+
 def get_class_display_name(klass):
-    return re.sub(r"([a-z\d])([A-Z])([a-z\d])", lambda m: "%s %s%s"%(m.group(1), m.group(2).lower(), m.group(3)), klass.__name__)
+    _ = getattr(builtins, "_", lambda s: s)
+    return _(format_class_name(klass.__name__))
