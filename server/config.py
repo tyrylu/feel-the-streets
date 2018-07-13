0d918b69a438c6f5b444a8df290e3c24460e991d
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-07-13 17:33:30 +0200

    Use an absolute path to the database file.

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(os.path.join(os.path.dirname(__file__), "server.db"))
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(os.path.join(os.path.abspath(os.path.dirname(__file__)), "server.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AMQP_BROKER_URL = os.environ.get("AMQP_BROKER_URL", "amqp://app:FeelTheStreets@trycht.cz")
     ENQUEUE_RETRIES = 5
