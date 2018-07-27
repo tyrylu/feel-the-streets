f492144181ea35d38131c8d4ec6511b8e24e392a
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-07-27 17:07:54 +0200

    Do not fail the entire update sequence when an area fails to update. Of course, it should not happen, however there's a pretty good chance that the rest will work, so allow them to be updated nonetheless.

--- server.wsgi
+++
@@ -1,4 +0,0 @@
-import sys, logging
-sys.stdout = sys.stderr
-logging.basicConfig(stream=sys.stderr)
-from server import app as application
--- server/tasks.py
+++ server/tasks.py
@@ -19,42 +19,47 @@ def create_database_task(area_name):
         chan.exchange_declare(area_name, exchange_type="fanout", durable=True)
     log.info("Generation sequence complete.")
 
+def do_area_update(area):
+    log.info("Processing area %s.", area.name)
+    area.state = AreaState.getting_changes
+    db.session.commit()
+    processor = ChangeActionProcessor(area.name)
+    first = True
+    for change in processor.new_semantic_changes(date=area.newest_osm_object_timestamp):
+        if first:
+            area.state = AreaState.applying_changes
+            db.session.commit()
+            first = False
+        entity = processor._db.apply_change(change)
+        valid = True
+        if entity:
+            try:
+                entity.create_osm_entity()
+            except Exception as ex:
+                log.warning("Application of change %s resulted in creation of an invalid entity.", change)
+                log.exception(ex)
+                valid = False
+        if not valid:
+            processor._db.rollback()
+        else:
+            msg_bin = pickle.dumps(change, protocol=pickle.HIGHEST_PROTOCOL)
+            huey.storage.channel.basic_publish(area.name, body=msg_bin, properties=pika.BasicProperties(delivery_mode=2), routing_key="")
+            processor._db.commit()
+    processor._db.close()
+    area.state = AreaState.updated
+    if not processor.newest_timestamp.startswith("1970"):
+        area.newest_osm_object_timestamp = processor.newest_timestamp
+    db.session.commit()
+    log.info("Geometry difference checks required retrieving %s objects.", processor._translator.manager.cached_total)
+    processor._translator.manager.remove_temp_data()
+    log.info("Finished processing area %s.", area.name)
+
 @huey.periodic_task(crontab(hour=22, minute=39))
 def update_area_databases_task():
     _configure_logging("update")
     for area in Area.query.filter_by(state=AreaState.updated):
-        log.info("Processing area %s.", area.name)
-        area.state = AreaState.getting_changes
-        db.session.commit()
-        processor = ChangeActionProcessor(area.name)
-        first = True
-        for change in processor.new_semantic_changes(date=area.newest_osm_object_timestamp):
-            if first:
-                area.state = AreaState.applying_changes
-                db.session.commit()
-                first = False
-            entity = processor._db.apply_change(change)
-            valid = True
-            if entity:
-                try:
-                    entity.create_osm_entity()
-                except Exception as ex:
-                    log.warning("Application of change %s resulted in creation of an invalid entity.", change)
-                    log.exception(ex)
-                    valid = False
-            if not valid:
-                processor._db.rollback()
-            else:
-                msg_bin = pickle.dumps(change, protocol=pickle.HIGHEST_PROTOCOL)
-                huey.storage.channel.basic_publish(area.name, body=msg_bin, properties=pika.BasicProperties(delivery_mode=2), routing_key="")
-                processor._db.commit()
-        processor._db.close()
-        area.state = AreaState.updated
-        if not processor.newest_timestamp.startswith("1970"):
-            area.newest_osm_object_timestamp = processor.newest_timestamp
-        db.session.commit()
-        log.info("Geometry difference checks required retrieving %s objects.", processor._translator.manager.cached_total)
-        processor._translator.manager.remove_temp_data()
-        log.info("Finished processing area %s.", area.name)
-        
-
+        try:
+            do_area_update(area)
+        except Exception as ex:
+            log.error("Updating area %s failed.", Area.name)
+            log.exception(ex)
