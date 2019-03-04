42a9e4762a51602238746618e05bf88cce81a838
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-03-04 17:17:08 +0100

    Rename server_rs to its proper name when we decided that the rewrite is good enough to be merged to master.

--- server/area_database_updater/generators/address_aware.py
+++
@@ -1,8 +0,0 @@
-from .utils import create_address
-from .generator import Generator
-
-class AddressAwareGenerator(Generator):
-    def _prepare_properties(self, entity_spec, props, record):
-        props = super()._prepare_properties(entity_spec, props, record)
-        props["address"] = create_address(props)
-        return props
--- server/huey_creation.py
+++
@@ -1,5 +0,0 @@
-import os
-from . import app
-
-def get_huey_kwargs():
-    return dict(broker_url=app.config["AMQP_BROKER_URL"], consume=os.environ.get("CONSUME", "False") == "True")
