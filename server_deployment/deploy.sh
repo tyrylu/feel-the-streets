499dfe89274344b838f5563ad16a7c3656065b98
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-04-06 15:05:57 +0200

    Fix deployment script.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 mkdir build
-cp target/release/{server,worker,create_area,interpret_area_changes} build/ \;
+cp target/release/{server,worker,create_area,interpret_area_changes} build/
 find server -name '*.yml' -exec cp {} build/ \;
 rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/home/travis/
