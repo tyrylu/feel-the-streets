1477765c4b2dac7cdbd0cb02742e19783f9b055a
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-04-06 16:27:36 +0200

    We need the rocket fongiguration as well.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -2,4 +2,5 @@
 mkdir build
 cp target/release/{server,worker,create_area,interpret_area_changes} build/
 find server -name '*.yml' -exec cp {} build/ \;
+find server -name '*.toml' -exec cp {} build/ \;
 rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/home/travis/
