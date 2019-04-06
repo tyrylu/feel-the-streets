54b44ae5944e190f45eebefe1e2a8c4f60b64976
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-04-06 18:46:05 +0200

    Tweak deployment script.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir build
 cp target/release/{server,worker,create_area,interpret_area_changes} build/
+cp server/Rocket.toml build/
 find server -name '*.yml' -exec cp {} build/ \;
-find server -name '*.toml' -exec cp {} build/ \;
 rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/home/travis/
