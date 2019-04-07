90d4e4afec0810a46c5f1319b13d46722067fde3
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-04-07 10:59:29 +0200

    Deploy to the final destination, add the services.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -3,4 +3,4 @@ mkdir build
 cp target/release/{server,worker,create_area,interpret_area_changes} build/
 cp server/Rocket.toml build/
 find server -name '*.yml' -exec cp {} build/ \;
-rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/home/travis/
+rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
