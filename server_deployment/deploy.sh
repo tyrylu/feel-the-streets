7767ec016eb334806e696317b94ca02dba427b95
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-04-10 08:51:54 +0200

    Keep also the debug versions, in case we need them.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -3,4 +3,6 @@ mkdir build
 cp target/release/{server,worker,create_area,interpret_area_changes} build/
 cp server/Rocket.toml build/
 find server -name '*.yml' -exec cp {} build/ \;
+mkdir build/debug
+cp target/debug/{server,worker,create_area,interpret_area_changes} build/debug/
 rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
