ca46c454097b68e847f7ab0cfb20145aca8a2f69
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-05-06 07:22:34 +0200

    Remove the database before recreation.

--- server/src/bin/recreate_all_areas.rs
+++ server/src/bin/recreate_all_areas.rs
@@ -2,11 +2,13 @@ extern crate server;
 use server::{background_tasks::area_db_creation, Result, area::Area};
 use std::time::Instant;
 use diesel::{Connection, SqliteConnection};
+use std::fs;
 
 fn main() -> Result<()> {
     server::init_logging();
     let server_conn = SqliteConnection::establish("server.db")?;
     for area in Area::all(&server_conn)? {
+        fs::remove_file(&format!("{}.db", area.name))?;
         println!("Recreating area {}...", area.name);
     let start = Instant::now();
     area_db_creation::create_area_database(&area.name)?;
