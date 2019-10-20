2c84574a4c3f8c2a638118592742f6a3f11bf569
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-10-20 07:47:39 +0200

    Send a debug build to help catching the set_error call.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir build
-cp target/release/{server,worker,create_area,interpret_area_changes,recreate_all_areas} build/
+cp target/debug/{server,worker,create_area,interpret_area_changes,recreate_all_areas} build/
 cp Rocket.toml build/
 find . -name '*.yml' -exec cp {} build/ \;
 rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
