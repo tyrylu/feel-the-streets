26dfe369c5f2b888fd218fb03dd531bf099f4d63
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2019-12-07 09:02:47 +0100

    Proper counting in the usage stats.

--- oesc/src/view_field_usage.rs
+++ oesc/src/view_field_usage.rs
@@ -9,6 +9,7 @@ pub fn view_field_usage(entity: String, field: String) -> Result<()> {
     let server_conn = SqliteConnection::establish("server.db")?;
     let mut values = HashMap::new();
     let mut null_values = 0;
+    let mut nonnull_values = 0;
     for area in Area::all(&server_conn)? {
         println!("Processing area {} (id {})...", area.name, area.osm_id);
         let area_db = AreaDatabase::open_existing(area.osm_id, true)?;
@@ -20,13 +21,14 @@ if val.is_null() {
     null_values += 1;
 }
 else {
+    nonnull_values += 1;
     let val_str = val.to_string();
     *values.entry(val_str).or_insert(0) += 1;
 }
         }
 println!("Area processed successfully.");
     }
-    println!("{} entities had the field, {} did not.", values.len(), null_values);
+    println!("{} entities had the field, {} did not. The entities had {} distinct values of the field.", nonnull_values, null_values, values.len());
     println!("Values summary follows.");
     for (val, occurrences) in values.iter() {
         println!("{} occurs {} times.", val, occurrences);
