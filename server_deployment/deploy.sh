e48e8d58add9b6700a6a3a05b97964af291702a4
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-01-28 14:03:58 +0100

    Use the new compression options for rsync.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -4,4 +4,4 @@ mkdir build
 cp target/release/{oesc,server,worker,create_area,interpret_area_changes,recreate_all_areas} build/
 cp Rocket.toml build/
 find . -name '*.yml' -exec cp {} build/ \;
-rsync -rvzh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
+rsync -zzrvh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
