54377640ec5c9299b2069cb9e215009512ade4c1
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-02-02 17:40:28 +0100

    Fix indent.

--- app/services.py
+++ app/services.py
@@ -30,10 +30,9 @@ def create_sound():
 
 def create_speech():
     if platform.system() == "Linux":
-
         # This hack inserts the speech dispatcher output to the known outputs for accessible_output2 and gets rid of the espeak one, because it does not athere to the output construction protocol, e. g. it does not throw OutputError as it should.
-    accessible_output2.outputs.__dict__["speech_dispatcher_output"] = speech_dispatcher_output
-    del accessible_output2.outputs.__dict__["e_speak"]
+        accessible_output2.outputs.__dict__["speech_dispatcher_output"] = speech_dispatcher_output
+        del accessible_output2.outputs.__dict__["e_speak"]
     # Now, we can rely on the standard automatic output selection.
     return auto.Auto()
 
