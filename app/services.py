f3bd19c613e5383e5ae5280d72b834d9f7f6f5d1
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-02-02 17:39:21 +0100

    Fix Linux output setup.

--- app/services.py
+++ app/services.py
@@ -1,10 +1,11 @@
 import sys
 import os
+import platform
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import appdirs
 import accessible_output2.outputs
-from accessible_output2.outputs import auto
+from accessible_output2.outputs import auto, e_speak
 from .di import Singleton
 from .sound_manager import SoundManager, SoundProperties
 from .map import Map
@@ -28,8 +29,12 @@ def create_sound():
     return mgr
 
 def create_speech():
-    # This hack inserts the speech dispatcher output to the known outputs for accessible_output2 and then returns the automatic output normally.
+    if platform.system() == "Linux":
+
+        # This hack inserts the speech dispatcher output to the known outputs for accessible_output2 and gets rid of the espeak one, because it does not athere to the output construction protocol, e. g. it does not throw OutputError as it should.
     accessible_output2.outputs.__dict__["speech_dispatcher_output"] = speech_dispatcher_output
+    del accessible_output2.outputs.__dict__["e_speak"]
+    # Now, we can rely on the standard automatic output selection.
     return auto.Auto()
 
 speech = Singleton(factory=create_speech)
