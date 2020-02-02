8a706927b1c5349067267acecf0d5c7636b73955
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-02-02 23:56:00 +0100

    Escape a dot in a bash glob pattern specifying the files to copy.

--- server_deployment/deploy.sh
+++ server_deployment/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 mkdir build
-cp target/release/{oesc,server,worker,create_area,recreate_all_areas, libosm_db.so} build/
+cp target/release/{oesc,server,worker,create_area,recreate_all_areas,libosm_db\.so} build/
 cp Rocket.toml build/
 find . -name '*.yml' -exec cp {} build/ \;
 rsync -zzrvh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
