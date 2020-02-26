2f1100548d4bce11cb175329fac2ac72cb5aee48
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-02-26 19:16:59 +0100

    Deploy the window exes as well (at least they're somewhere), use bash for executing the deploy script.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 set -e
 mkdir build
+if [ "$TRAVIS_OS_NAME" = "windows" ]
+then
+cp target/release/*.exe build/
+else
 cp target/release/{oesc,server,worker,create_area,recreate_all_areas,libosm_db\.so} build/
+fi
 cp Rocket.toml build/
 find . -name '*.yml' -exec cp {} build/ \;
 rsync -zzrvh -e "ssh -p $DEPLOY_TO_PORT" build/ travis@$DEPLOY_TO:/srv/feel-the-streets/
