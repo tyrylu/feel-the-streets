2231412d6c516ed97a9f974bc729023625052f05
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-02-27 23:20:15 +0100

    User the ssh agent after all.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,5 +1,7 @@
 openssl aes-256-cbc -K $encrypted_68d9c3eaf1da_key -iv $encrypted_68d9c3eaf1da_iv -in travis.enc -out travis -d
 chmod 600 ./travis
+eval 'ssh-agent -s'
+ssh-add travis
 # On the windows builder, we don't have any python3 and we need some.
 if [ "$TRAVIS_OS_NAME" = "windows" ]
 then
