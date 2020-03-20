7e38e582049179d90497c1e4355ba720d596847e
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-03-21 00:25:53 +0100

    Forgotten migration.

---
+++ server/migrations/2020-03-20-230944_add_db_size/down.sql
@@ -0,0 +1,17 @@
+BEGIN;
+CREATE TABLE "new_areas" (
+        id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        state VARCHAR(8) NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME, 
+        newest_osm_object_timestamp TEXT,
+        osm_id INTEGER NOT NULL default 0,
+        PRIMARY KEY (id),
+        UNIQUE (name),
+        CONSTRAINT areastate CHECK (state IN ('creating', 'getting_changes', 'applying_changes', 'updated'))
+);
+INSERT INTO new_areas SELECT id, name, state, created_at, updated_at, newest_osm_object_timestamp, osm_id FROM areas;
+DROP TABLE areas;
+ALTER TABLE new_areas RENAME TO areas;
+COMMIT;
