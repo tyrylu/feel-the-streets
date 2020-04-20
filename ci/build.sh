55457b007a52063e0e956a4b76f1cebe4ec05104
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-04-20 14:20:52 +0200

    Try calling pip through python, it might help on travis...

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@ cargo build --verbose --all
 # On Windows, build the executable binary.
 if [ "$TRAVIS_OS_NAME" = "windows" ]
 then
-pip install -r app_requirements.txt
+python -m pip install -r app_requirements.txt
 # We need osm_db module in the right place.
 mv target/release/osm_db.dll osm_db.pyd
 pyinstaller run_app.spec
