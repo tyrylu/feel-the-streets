3dc98e75dbf6541540daf93091fa9123d3029d03
U-ORION\lukas <C:\Users\lukas\AppData\Roaming\The Bat!>
2020-07-25 18:47:55 +0200

    Locale update and an error fixed in an unneded source code, but the message extraction was failing on it.

--- app/entities/vehicle.py
+++ app/entities/vehicle.py
@@ -11,7 +11,7 @@ class Vehicle(Entity):
         x, y, z = self.cartesian_position
         self._sound_channel = sound().play(self.sound, x=x, y=y, z=z)
     
-       def update_position(self, time_delta):
+    def update_position(self, time_delta):
         meters_per_second = self.speed/3.6
         distance = meters_per_second*time_delta
         self.move_by(distance)
