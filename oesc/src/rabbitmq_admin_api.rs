4bf6186f98c6cc70bcae7d7cca1bae9bde10dedd
U-ORION\lukas <C:\Users\lukas\AppData\Roaming\The Bat!>
2020-08-26 14:15:38 +0200

    Try using ureq for the Rabbitmq API, it might handle basic auth better.

--- oesc/src/rabbitmq_admin_api.rs
+++ oesc/src/rabbitmq_admin_api.rs
@@ -25,6 +25,6 @@ impl Client {
 
     pub fn get_queues_bound_to_exchange(&self, vhost: &str, exchange: &str) -> Result<Vec<ExchangeBinding>> {
         let final_url = self.rabbitmq_http_api_base.join(&format!("exchanges/{}/{}/source", vhost, exchange))?;
-        Ok(reqwest::blocking::get(final_url.as_str())?.json()?)
+        Ok(ureq::get(final_url.as_str()).call().into_json_deserialize()?)
     }
 }
