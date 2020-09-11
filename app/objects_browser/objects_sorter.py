fc7b0183961e2604cee0ab772a2bbb7366b13667
U-ORION\lukas <C:\Users\lukas\AppData\Roaming\The Bat!>
2020-09-11 23:50:51 +0200

    Forgotten file.

---
+++ app/objects_browser/objects_sorter.py
@@ -0,0 +1,30 @@
+from PySide2.QtCore import QThread, Signal
+from ..services import config
+from ..humanization_utils import format_number, describe_entity
+from ..geometry_utils import distance_between, bearing_to
+
+
+class ObjectsSorter(QThread):
+    objects_sorted = Signal(tuple)
+
+    def __init__(self, unsorted_objects, person):
+        super().__init__(None)
+        self._unsorted_objects = unsorted_objects
+        self._person = person
+
+    def run(self):
+        objects = []
+        items_data = []
+        for obj in self._unsorted_objects:
+            closest_latlon = self._person.closest_point_to(obj.geometry)
+            cur_distance = distance_between(closest_latlon, self._person.position)
+            objects.append((cur_distance, obj, closest_latlon))
+        objects.sort(key=lambda e: e[0])
+        for dist, obj, closest in objects:
+            bearing = bearing_to(self._person.position, closest)
+            rel_bearing = (bearing - self._person.direction) % 360
+            rel_bearing = format_number(rel_bearing, config().presentation.angle_decimal_places)
+            dist = format_number(dist, config().presentation.distance_decimal_places)
+            items_data.append((describe_entity(obj), dist, rel_bearing))
+        self.objects_sorted.emit((objects, items_data))
+        
