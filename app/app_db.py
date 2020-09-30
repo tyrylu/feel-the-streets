4746ea917d24e362a5063fa3acda2c7757b95d80
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-09-30 16:03:44 +0200

    Fix last location saving.

--- app/app_db.py
+++ app/app_db.py
@@ -10,9 +10,12 @@ class AppDb:
         self._db = sqlite3.connect(db_path)
         self._db.execute(MAYBE_CREATE_BOOKMARKS)
         self._db.execute(MAYBE_CREATE_LOCATIONS)
+        res = self._db.execute("SELECT area, id from last_locations")
+        self._last_location_ids = dict(res.fetchall())
 
     def add_bookmark(self, mark):
         self._db.execute("INSERT INTO bookmarks (name, area, latitude, longitude), VALUES (?, ?, ?, ?)", (makr.name, mark.area, mark.latitude, mark.longitude))
+        self._db.commit()
 
     def bookmarks_for_area(self, area):
         cursor = self._db.execute("SELECT id, name, latitude, longitude FROM bookmarks WHERE area = ?", (area,))
@@ -23,6 +26,7 @@ class AppDb:
 
     def remove_bookmark(self, bookmark_id):
         self._db.execute("DELETE FROM bookmarks WHERE id = ?", (bookmark_id,))
+        self._db.commit()
 
     def last_location_for(self, area_id):
         cursor = self._db.execute("SELECT id, latitude, longitude FROM last_locations where area = ? LIMIT 1", (area_id,))
@@ -33,4 +37,10 @@ class AppDb:
         return LastLocation(id=id, area=area_id, latitude=latitude, longitude=longitude)
 
     def update_last_location_for(self, area_id, lat, lon):
-        self._db.execute("REPLACE INTO last_locations (area, latitude, longitude) VALUES (?, ?, ?)", (area_id, lat, lon))
+        location_id = self._last_location_ids.get(area_id, None)
+        if location_id:
+            self._db.execute("REPLACE INTO last_locations (id, area, latitude, longitude) VALUES (?, ?, ?, ?)", (location_id, area_id, lat, lon))
+        else:
+            self._db.execute("INSERT INTO last_locations (area, latitude, longitude) VALUES (?, ?, ?)", (area_id, lat, lon))
+            self._last_location_ids[area_id] = self._db.execute("SELECT id FROM last_locations ORDER BY id DESC LIMIT 1").fetchone()[0]
+        self._db.commit()
