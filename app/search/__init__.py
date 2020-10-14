bbb5f645e1a9ba1173eeea9af9f856ff7b120b93
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-10-14 11:36:20 +0200

    Refactor search query creation so it can be reused in the future search by name command.

--- app/search/__init__.py
+++ app/search/__init__.py
@@ -6,29 +6,15 @@ from .query_executor import QueryExecutor
 from .search_indicator import SearchIndicator
 from ..geometry_utils import xy_ranges_bounding_square
 
-def get_query_from_user(parent):
-    entities = all_known_discriminators()
-    name_mapping = {}
-    for entity in entities:
-        name_mapping[get_class_display_name(entity)] = entity
-    entity, ok = QInputDialog.getItem(parent, _("Search class"), _("Select the class to search"), sorted(name_mapping.keys()), editable=False)
-    if not ok:
-        return
-    discriminator = name_mapping[entity]
-    conditions_dialog = SpecifySearchConditionsDialog(parent, entity=discriminator)
-    if conditions_dialog.exec_() != SpecifySearchConditionsDialog.Accepted:
-        return
-    conditions = conditions_dialog.create_conditions()
-    distance = float("inf")
+def create_query(discriminator, distance, conditions):
     query = EntitiesQuery()
-    if conditions_dialog.distance:
-        distance = conditions_dialog.distance
+    if distance < float("inf"):
         min_x, min_y, max_x, max_y = xy_ranges_bounding_square(position, distance*2)
         query.set_rectangle_of_interest(min_x, max_x, min_y, max_y)
     # We have no easy way how to determine the children of a class, so we would have to iterate through all the classes anyway.
     # There's an assumption that the excluded classes will not be as numerous (for example almost anything is named), so find the exceptions instead.
     excluded_discriminators = set()
-    for candidate in entities:
+    for candidate in all_known_discriminators():
         found = False
         metadata = EntityMetadata.for_discriminator(candidate)
         while metadata:
@@ -41,5 +27,21 @@ def get_query_from_user(parent):
     query.set_excluded_discriminators(list(excluded_discriminators))
     for condition in conditions:
         query.add_condition(condition)
+    return query
+
+def get_query_from_user(parent):
+    entities = all_known_discriminators()
+    name_mapping = {}
+    for entity in entities:
+        name_mapping[get_class_display_name(entity)] = entity
+    entity, ok = QInputDialog.getItem(parent, _("Search class"), _("Select the class to search"), sorted(name_mapping.keys()), editable=False)
+    if not ok:
+        return
+    discriminator = name_mapping[entity]
+    conditions_dialog = SpecifySearchConditionsDialog(parent, entity=discriminator)
+    if conditions_dialog.exec_() != SpecifySearchConditionsDialog.Accepted:
+        return
+    distance = conditions_dialog.distance or float("inf")
+    query = create_query(discriminator, distance, conditions_dialog.create_conditions())
     return query, distance
     
