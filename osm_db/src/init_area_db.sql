c13a30e90f0a6b38490beb9f1037eaa44fb0b93f
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-11-02 15:05:36 +0100

    Create an index by the entity name, which improves exact name queries and name based queries overall, unfortunately not the like queries.

--- osm_db/src/init_area_db.sql
+++ osm_db/src/init_area_db.sql
@@ -6,4 +6,5 @@ CREATE TABLE entities (
 	effective_width FLOAT);
 SELECT AddGeometryColumn("entities", "geometry", 4326, "GEOMETRY", 2, 1);
 SELECT CreateSpatialIndex('entities', 'geometry');
+CREATE INDEX idx_entity_by_name on entities(json_extract(data, "$.name"));
 CREATE TABLE entity_relationships (parent_id VARCHAR(16) REFERENCES entities(id) ON DELETE CASCADE, child_id VARCHAR(16) REFERENCES entities(id) ON DELETE CASCADE, PRIMARY KEY (parent_id, child_id));
