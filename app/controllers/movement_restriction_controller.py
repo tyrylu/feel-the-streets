d860d69170e4bdd1a0ac8f7dcd8b39370db3a408
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2020-11-04 14:37:23 +0100

    Allow the pre enter and other pre hooks to cancel the current movement without sending the move_rejected signal.

--- app/controllers/movement_restriction_controller.py
+++ app/controllers/movement_restriction_controller.py
@@ -1,4 +1,4 @@
-from ..entities import entity_pre_leave, entity_pre_enter
+from ..entities import entity_pre_leave, entity_pre_enter, MoveValidationResult
 from ..services import config
 from ..entity_utils import filter_important_roads, is_footway, get_last_important_road
 from ..geometry_utils import get_meaningful_turns, get_smaller_turn
@@ -12,11 +12,11 @@ class MovementRestrictionController:
 
     def _entity_pre_enter(self, sender, enters):
         if sender is not self._restricted_entity or not config().navigation.try_avoid_sidewalks:
-            return True # You can enter whatever you wish.
+            return MoveValidationResult.accept # You can enter whatever you wish.
         for place in enters:
             if place.is_road_like and is_footway(place) and not self._should_allow_entrance_to_footway(place):
-                return False
-        return True
+                return MoveValidationResult.reject
+        return MoveValidationResult.accept
 
     def _should_allow_entrance_to_footway(self, footway):
         if not self._restricted_entity.inside_of_roads:
@@ -31,11 +31,11 @@ class MovementRestrictionController:
 
     def _entity_pre_leave(self, sender, leaves):
         if not config().navigation.disallow_leaving_roads or sender is not self._restricted_entity:
-            return True
+            return MoveValidationResult.accept
         to_be_left_roads = [e for e in leaves if e.is_road_like]
         if not to_be_left_roads:
-            return True # We're only interested in leaving roads
+            return MovementRestrictionController.accept # We're only interested in leaving roads
         remaining_road_like = [thing for thing in filter_important_roads(sender.inside_of_roads) if thing not in to_be_left_roads]
         # Are we leaving a road like thing and no other road like thing remains?
-        return len(remaining_road_like) > 0
+        return MoveValidationResult.accept if len(remaining_road_like) > 0 else MoveValidationResult.reject
         
