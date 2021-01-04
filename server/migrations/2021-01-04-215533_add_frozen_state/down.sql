d87f2feb0ca46fee82722199c5a49655d04b46b8
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2021-01-04 23:24:48 +0100

    Add the new stuff which was actually forgotten before

---
+++ server/migrations/2021-01-04-215533_add_frozen_state/down.sql
@@ -0,0 +1,16 @@
+BEGIN;
+CREATE TABLE "new_areas" (
+        id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        state VARCHAR(8) NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME, newest_osm_object_timestamp TEXT, osm_id INTEGER NOT NULL DEFAULT 0, db_size INTEGER NOT NULL DEFAULT 0,
+        PRIMARY KEY (id),
+        UNIQUE (name),
+        CONSTRAINT areastate CHECK (state IN ('creating', 'getting_changes', 'applying_changes', 'updated'))
+);
+UPDATE areas SET state = "updated" where state = "frozen";
+INSERT INTO new_areas SELECT * from AREAS;
+DROP TABLE areas;
+ALTER TABLE new_areas RENAME TO areas;
+COMMIT;
