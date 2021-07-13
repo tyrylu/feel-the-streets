4d26d438401195a6ecc48e1277603218e04ac359
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2021-07-13 18:06:45 +0200

    Forgotten file which we do not need

--- app/amqp_queue_naming.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-def get_client_queue_name(client_id: str, area_osm_id: int) -> str:
-    hashable = "{0}{1}".format(client_id, area_osm_id).encode("UTF-8")
-    return hashlib.new("sha3_256", hashable).hexdigest()
