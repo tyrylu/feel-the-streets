36fd039975d0375019384b11f65b30e55ff2d7c0
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2021-09-16 00:21:43 +0200

    Show entity count statistics on the area detail page

--- server/src/ui_routes.rs
+++ server/src/ui_routes.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use crate::area::Area;
 use crate::{DbConn, Result};
+use osm_db::AreaDatabase;
 use redis_api::ChangesStream;
 use rocket_dyn_templates::Template;
 
@@ -46,17 +47,30 @@ pub async fn area_detail(area_id: i32, conn: DbConn) -> Result<Template> {
         area: Area,
         change_counts: HashMap<String, u64>,
         redownload_requests: Vec<String>,
+        entity_count: usize,
+        entity_relationship_count: usize,
+        entity_counts: HashMap<String, usize>,
+        relationship_counts: HashMap<String, usize>,
     }
     let area = conn.run(move |c| Area::find_by_id(area_id, c)).await?;
     let mut stream = ChangesStream::new_from_env(area.osm_id)?;
     let redownload_requests = stream.all_redownload_requests()?;
     let change_counts = stream.all_change_counts()?;
+    let area_db = AreaDatabase::open_existing(area.osm_id, true)?;
+    let entity_count = area_db.num_entities()?;
+    let entity_relationship_count = area_db.num_entity_relationships()?;
+    let entity_counts = area_db.get_entity_counts_by_discriminator()?;
+    let relationship_counts = area_db.get_entity_relationship_counts_by_kind()?;
     Ok(Template::render(
         "area_detail",
         Tctxt {
             area,
             change_counts,
             redownload_requests,
+            entity_count,
+            entity_relationship_count,
+            entity_counts,
+            relationship_counts,
         },
     ))
 }
