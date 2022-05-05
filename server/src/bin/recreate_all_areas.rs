297815eef19e7dacec0bd187ea0a832dc3577a69
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2022-05-05 15:48:40 +0200

    Don't fail area recreation if a file can not be deleted, e. g. the process was interrupted

--- server/src/bin/recreate_all_areas.rs
+++ server/src/bin/recreate_all_areas.rs
@@ -2,6 +2,7 @@ extern crate server;
 use diesel::{Connection, SqliteConnection};
 use server::{area::Area, background_tasks::area_db_creation, Result};
 use std::fs;
+use std::path::Path;
 use std::time::Instant;
 
 fn main() -> Result<()> {
@@ -9,7 +10,10 @@ fn main() -> Result<()> {
     server::init_logging();
     let server_conn = SqliteConnection::establish("server.db")?;
     for area in Area::all(&server_conn)? {
-        fs::remove_file(&format!("{}.db", area.osm_id))?;
+        let area_file = format!("{}.db", area.osm_id);
+        if Path::new(&area_file).exists() {
+            fs::remove_file(&area_file)?;
+        }
         println!("Recreating area {}...", area.name);
         let start = Instant::now();
         area_db_creation::create_area_database(area.osm_id)?;
