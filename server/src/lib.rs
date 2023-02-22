5aafd527586338c60de089d75c3f3f9c3abe6469
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2023-02-22 17:54:40 +0100

    Don't bother emitting colors when the logging output does not go to a terminal

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -10,6 +10,7 @@ pub mod names_cache;
 pub mod ui_routes;
 
 pub use error::Error;
+use is_terminal::IsTerminal;
 use std::sync::{Arc, Mutex};
 use tera::Tera;
 use tracing_subscriber::prelude::*;
@@ -26,15 +27,20 @@ pub fn init_logging() {
     let filter_layer = tracing_subscriber::EnvFilter::new(
         std::env::var("FTS_LOG").unwrap_or_else(|_| "info".into()),
     );
+    let use_ansi = std::io::stdout().is_terminal();
     if std::env::var("INVOCATION_ID").is_ok() {
         tracing_subscriber::registry()
             .with(filter_layer)
-            .with(tracing_subscriber::fmt::layer().without_time())
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .without_time()
+                    .with_ansi(false),
+            )
             .init();
     } else {
         tracing_subscriber::registry()
             .with(filter_layer)
-            .with(tracing_subscriber::fmt::layer())
+            .with(tracing_subscriber::fmt::layer().with_ansi(use_ansi))
             .init();
     }
 }
