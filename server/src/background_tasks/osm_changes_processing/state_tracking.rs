6589b1281b21843ced9b48408ae6770a4a60d9fa
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2023-05-06 13:24:09 +0200

    Use a default for the latest change number if we don't have one

--- server/src/background_tasks/osm_changes_processing/state_tracking.rs
+++ server/src/background_tasks/osm_changes_processing/state_tracking.rs
@@ -1,13 +1,20 @@
 use crate::Result;
+use log::info;
 use std::fs;
+use std::path::Path;
 
 const LATEST_SN_FILE: &str = "latest_processed.sn";
 
 pub(crate) fn read_latest_sequence_number() -> Result<u32> {
-    let content = fs::read_to_string(LATEST_SN_FILE)?;
-    Ok(content
-        .parse()
-        .expect("Could not parse latest sequence number"))
+    if Path::new(LATEST_SN_FILE).exists() {
+        let content = fs::read_to_string(LATEST_SN_FILE)?;
+        Ok(content
+            .parse()
+            .expect("Could not parse latest sequence number"))
+    } else {
+        info!("Latest processed sequence number not stored, using default.");
+        Ok(5_400_000)
+    }
 }
 
 pub(crate) fn save_latest_sequence_number(number: u32) -> Result<()> {
