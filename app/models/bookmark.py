5d9fef4e392f408fd6acac9898f49191de34e5e8
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2023-08-23 17:10:24 +0200

    Update transitive deps

--- app/models/bookmark.py
+++ app/models/bookmark.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class Bookmark(BaseModel):
     __tablename__ = "bookmarks"
     id: int
-    area: str
+    area: int
     name: str
     latitude: float
     longitude: float
