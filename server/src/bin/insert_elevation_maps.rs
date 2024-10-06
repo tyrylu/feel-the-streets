336099ab4c107030a0b5224568cba7858902739f
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2024-10-06 17:09:07 +0200

    Introduce a script to fill the elevation maps

---
+++ server/src/bin/insert_elevation_maps.rs
@@ -0,0 +1,17 @@
+use log::info;
+use osm_db::AreaDatabase;
+use server::{area::Area, background_tasks::area_db_creation, db, Result};
+
+fn main() -> Result<()> {
+    let _dotenv_path = dotenvy::dotenv()?;
+    server::init_logging();
+    let db_conn = db::connect_to_server_db()?;
+    for area in Area::all(&db_conn)? {
+        let bounds = area.bounds(&db_conn)?;
+        info!("Getting elevation map for {}, bounds: {:?}", area.name, bounds);
+        let elevation_map = area_db_creation::elevation_map_for_bounds(&bounds)?;
+        let area_db = AreaDatabase::open_existing(area.osm_id, true)?;
+        area_db.replace_elevation_map(&elevation_map)?;
+    }
+    Ok(())
+}
