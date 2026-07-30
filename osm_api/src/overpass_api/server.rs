462bb0ca64b82237bba10c75b783ea7938edad74
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2026-07-30 08:41:07 +0200

    Try disabling gzip encoding

--- osm_api/src/overpass_api/server.rs
+++ osm_api/src/overpass_api/server.rs
@@ -210,6 +210,6 @@ impl Server {
 
     fn prepare_run_query(&self) -> RequestBuilder<WithBody> {
         let final_url = format!("{}/api/interpreter", self.url);
-        self.agent.post(&final_url)
+        self.agent.post(&final_url).header("Accept-Encoding", "")
     }
 }
